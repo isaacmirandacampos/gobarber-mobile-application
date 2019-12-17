61ae404aef1eb449ded38cf63d524741da3c80cd
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-12-17 14:29:41 -0300

    feature input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -3,11 +3,12 @@ import { Text } from 'react-native';
 
 import Background from '~/components/Background';
 // import { Container } from './styles';
+import Input from '~/components/Input';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Input style={{ marginTop: 10 }} />
     </Background>
   );
 }
