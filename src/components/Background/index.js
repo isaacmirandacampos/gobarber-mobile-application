e49386d7a8026977ba71face47ae3971e9e2120d
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-12-17 13:03:36 -0300

    configure routes and create background

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#cc3388'],
+})`
+  height: 100%;
+`;
